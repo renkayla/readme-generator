de2ac72e5d419ea3229f247b88c94c84b52b66d5
renkayla <116777951+renkayla@users.noreply.github.com>
2023-04-24 18:39:31 -0400

    index.js and generatemarkdown syntax

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# readme-generator
+# weather-dashboard
+  ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+  ## Description
+  
+  it is a dashboard for weather
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Tests](#tests)
+  - [Contributing](#contributing)
+  - [License](#license)
+  - [Questions
--- index.js
+++ index.js
@@ -1,13 +1,72 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please provide a short description of your project:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license should your project have?',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'installCommand',
+    message: 'What command should be run to install dependencies?',
+    default: 'npm install',
+  },
+  {
+    type: 'input',
+    name: 'testCommand',
+    message: 'What command should be run to run tests?',
+    default: 'npm test',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What does the user need to know about using the repo?',
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'What does the user need to know about contributing to the repo?',
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log('README.md file generated!')
+  );
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    const markdown = generateMarkdown(data);
+    writeToFile('README.md', markdown);
+  });
+}
 
 // Function call to initialize app
 init();
